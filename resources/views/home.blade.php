4f33a0769d2a00f7e75edd260b947f729a4673d2
quyencao <boy20195@gmail.com>
2017-08-05 11:03:53 +0700

    Use blade @component to test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,8 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in! <strong>USER</strong>
+                    @component('components.who')
+                    @endcomponent
                 </div>
             </div>
         </div>
